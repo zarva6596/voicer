8f607347f7466a6998f76e7a019320ab8bfce3f8
zarva6596 <zarva6596@gmail.com>
2022-06-24 01:10:36 +0300

    fixed images

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -37,6 +37,6 @@
 </template>
 
 <script setup>
-  const logo = 'assets/img/logos/logo.svg'
-  const cloud = 'assets/img/header/cloud.svg'
+  const logo = 'public/img/logos/logo.svg'
+  const cloud = 'public/img/header/cloud.svg'
 </script>
