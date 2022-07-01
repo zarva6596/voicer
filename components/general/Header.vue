51be106bda323796d726e402051a793fb371d5df
mykhailo <m.diakovych13@gmail.com>
2022-07-01 23:57:35 +0300

    try 2

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -3,7 +3,7 @@
     <div class="max-w-[1440px] mx-auto flex justify-between items-center py-6 px-4 lg:px-16">
       <div class="flex items-center">
         <nuxt-link class="mr-4" to="/">
-          <img src="~/assets/img/logos/logo.svg" alt="Voicer logo" />
+          <img src="~/public/img/logos/logo.svg" alt="Voicer logo" />
         </nuxt-link>
 
         <div>
